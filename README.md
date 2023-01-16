07a4882b38eee40c8f758d1a7ecb5faf1f7a3703
Jardon237 <ngwajude161@gmail.com>
2023-01-16 15:38:19 +0100

    requirements

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # travel-webapp
 this is the repository for s travel webapp
+
+# create virtual environment
+<virtualenv -p python 3>
+activate
+<source bin/activate>
+
---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jardon/dev/travel-webapp/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
