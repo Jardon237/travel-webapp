8b4539c9cb45d1a020380e891e38e18a2c9b342f
Jardon237 <ngwajude161@gmail.com>
2023-01-19 17:20:05 +0100

    link all views to urls and templates

--- bookings/views.py
+++ bookings/views.py
@@ -4,5 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+def seat(request):
+    return render (request, 'seat.html')
 
+def signin(request):
+    return render (request, 'signin.html')   
+
+def signup(request):
+    return render (request, 'signup.html')
+
+def available(request, *args, **kwargs):
+    return render(request, 'available.html')
 
